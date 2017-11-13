6c0b32f68c2c8b0373a3f24046f22c7bae75e4a8
katherineRamirez <oeillet8_5@hotmail.com>
2017-11-12 22:38:24 -0300

    twitter final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,57 @@
-var tuit = function(){
-    if(document.getElementById("message").value.length == 0){
-        document.getElementById('boton').setAttribute('disabled');
-    }else{
-        var mensaje = document.getElementById("message").value;
-        var tweet = document.getElementById("boton").value;
-    }
+// Función que se aplica al cargar la página
+window.onload = function() {
+
+// Llamando mis elementos del html
+
+var texto = document.getElementById("txt");
+var boton = document.getElementById("btn");
+var contador = document.getElementById("count");
+var nuevoContenedor = document.getElementById("contenido");
+
+// Creando función para el botón
+boton.addEventListener("click",function(){
+
+// Mostrar mensaje en el caso que no se ingrese ningún comentario
+    if (texto.value == "") {
+          alert("Escriba su comentario");
+        } else {
+
+// Creando un div contenedor con el texto rescatado de mi textarea
+        var contenedor_usuario = document.createElement("div");
+        contenedor_usuario.classList.add("class","clase_div_contenedor");
+        var titulo_usuario = document.createElement("p");
+        var text_titulo_usuario = document.createTextNode(texto.value);
+        var dateSpan = document.createElement("span");
+ 
+ // Asignando los hijos al padre
+        titulo_usuario.appendChild(text_titulo_usuario);
+        contenedor_usuario.appendChild(titulo_usuario);
+        contenedor_usuario.appendChild(dateSpan);
+        dateSpan.textContent = new Date();
+        nuevoContenedor.appendChild(contenedor_usuario);
+
+// Limpiando texto después de hacer click en el botón
+        document.getElementById("txt").value="";
+
 }
-function count_down(obj){
-    var element = document.getElementById("count1");
-    element.innerHTML = 140 - obj.value.length;
+
+// Función para el contador de forma regresiva y así ir cambiando los colores
+    function countDown(obj){
+    var contador = document.getElementById("count");
+    contador.innerHTML = 140 - obj.value.length;
 
     if (140 - obj.value.length == 130){
-        element.style.color = "red";
+        contador.style.color = "red";
     } else if(140 - obj.value.length == 120){
-        element.style.color = "green";
+        contador.style.color = "green";
 
     } else if(140 - obj.value.length == -1){
-        element.style.color = "pink";
+        contador.style.color = "pink";
     }
     
 }
+})
+}
+
+
+
